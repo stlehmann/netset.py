a1a4cbef23e0fee1abc8fb61ab98c41ca9bc5dc5
Stefan Lehmann <stlm@posteo.de>
2019-03-08 08:38:18 +0100

    netset.py: Change names list to ls and remove to rm.

--- netset.py
+++ netset.py
@@ -8,10 +8,10 @@ import wmi
 import pickle
 
 
-__version__ = '0.0.2'
+__version__ = "0.0.2"
 
 TIMEOUT_SECONDS = 120
-CONFIG_FILENAME = 'netset.pkl'
+CONFIG_FILENAME = "netset.pkl"
 
 
 # Save list function
@@ -19,10 +19,10 @@ list_ = list
 
 
 # static configuration
-Config = namedtuple('Config', 'name dhcp ip subnetmask gateway')
+Config = namedtuple("Config", "name dhcp ip subnetmask gateway")
 configs = {
-    'dhcp': Config('dhcp', True, None, None, None),
-    'static': Config('static', False, '192.168.1.110', '255.255.255.0', '192.168.1.0')
+    "dhcp": Config("dhcp", True, None, None, None),
+    "static": Config("static", False, "192.168.1.110", "255.255.255.0", "192.168.1.0"),
 }
 
 
@@ -33,17 +33,17 @@ def get_nic():
 
 
 def ip_address(nic):
-    ip = nic.wmi_property('IPAddress')
+    ip = nic.wmi_property("IPAddress")
     return ip.value[0]
 
 
 def subnetmask(nic):
-    subnet = nic.wmi_property('IPSubnet')
+    subnet = nic.wmi_property("IPSubnet")
     return subnet.value[0]
 
 
 def gateway(nic):
-    gateway = nic.wmi_property('DefaultIPGateway')
+    gateway = nic.wmi_property("DefaultIPGateway")
     try:
         return gateway.value[0]
     except TypeError:
@@ -51,7 +51,7 @@ def gateway(nic):
 
 
 def dhcp_enabled(nic):
-    dhcp = nic.wmi_property('DHCPEnabled')
+    dhcp = nic.wmi_property("DHCPEnabled")
     return dhcp.value
 
 
@@ -59,9 +59,11 @@ def config_active(config):
     nic = get_nic()
     if config.dhcp:
         return dhcp_enabled(nic)
-    return (config.ip == ip_address(nic) and
-            config.subnetmask == subnetmask(nic) and
-            config.gateway == gateway(nic))
+    return (
+        config.ip == ip_address(nic)
+        and config.subnetmask == subnetmask(nic)
+        and config.gateway == gateway(nic)
+    )
 
 
 def get_active_configs():
@@ -72,19 +74,21 @@ def get_active_configs():
     active_subnetmask = subnetmask(nic)
     active_gateway = gateway(nic)
 
-
     for key, config in configs.items():
         if active_dhcp and config.dhcp:
             yield config.name
         else:
-            if (active_ip, active_subnetmask, active_gateway) == \
-                    (config.ip, config.subnetmask, config.gateway):
+            if (active_ip, active_subnetmask, active_gateway) == (
+                config.ip,
+                config.subnetmask,
+                config.gateway,
+            ):
                 yield config.name
 
 
 def save_configs():
     global configs
-    with open(CONFIG_FILENAME, 'wb') as f:
+    with open(CONFIG_FILENAME, "wb") as f:
         pickle.dump(configs, f)
 
 
@@ -94,13 +98,13 @@ def cli():
 
 
 @cli.command()
-@click.argument('name')
-@click.option('--dhcp', is_flag=True, help='dhcp on')
-@click.option('--ip', '-i', help='ip address')
-@click.option('--mask', '-m', help='subnet mask')
-@click.option('--gateway', '-g', help='gateway')
+@click.argument("name")
+@click.option("--dhcp", is_flag=True, help="dhcp on")
+@click.option("--ip", "-i", help="ip address")
+@click.option("--mask", "-m", help="subnet mask")
+@click.option("--gateway", "-g", help="gateway")
 def add(name, dhcp, ip, mask, gateway):
-    """ add a config to list """
+    """Add a configuration."""
     global configs
     new_cfg = Config(name, dhcp, ip, mask, gateway)
     configs[name] = new_cfg
@@ -108,56 +112,53 @@ def add(name, dhcp, ip, mask, gateway):
 
 
 @cli.command()
-@click.argument('name')
-def remove(name):
-    """ Remove a config from list """
+@click.argument("name")
+def rm(name):
+    """Remove a config from list."""
     global configs
     configs.pop(name)
     save_configs()
 
 
 @cli.command()
-def list():
-
+def ls():
+    """List configurations."""
     active_configs = list_(get_active_configs())
 
     def list_data(x):
-        active = '*' if x.name in active_configs else ''
+        active = "*" if x.name in active_configs else ""
         if x.dhcp:
-            return '{}{}'.format(x.name, active), 'DHCP', '-', '-'
+            return "{}{}".format(x.name, active), "DHCP", "-", "-"
         else:
-            return '{}{}'.format(x.name, active), x.ip, x.subnetmask, x.gateway
+            return "{}{}".format(x.name, active), x.ip, x.subnetmask, x.gateway
 
     """ list available network configurations """
-    headers = ['Name', 'IP-Address', 'Subnetmask', 'Gateway']
+    headers = ["Name", "IP-Address", "Subnetmask", "Gateway"]
     table = [list_data(x) for _, x in configs.items()]
-    click.echo('\nNetset Profiles:\n')
+    click.echo("\nNetset Profiles:\n")
     click.echo(tabulate(table, headers))
 
 
 def _status():
     nic = get_nic()
-    s = 'DHCP\n' if dhcp_enabled(nic) else ''
-    s += ('IP: {ip}\nSubnetmask: {subnetmask}\n'
-          'Gateway: {gateway}'.format(
-              ip=ip_address(nic),
-              subnetmask=subnetmask(nic),
-              gateway=gateway(nic)
-          ))
-    click.echo('\nCurrent Network Status:\n')
+    s = "DHCP\n" if dhcp_enabled(nic) else ""
+    s += "IP: {ip}\nSubnetmask: {subnetmask}\n" "Gateway: {gateway}".format(
+        ip=ip_address(nic), subnetmask=subnetmask(nic), gateway=gateway(nic)
+    )
+    click.echo("\nCurrent Network Status:\n")
     click.echo(s)
 
 
 @cli.command()
 def status():
-    """ show current network status """
+    """Show current network status."""
     _status()
 
 
 @cli.command()
-@click.argument('config')
+@click.argument("config")
 def load(config):
-    """ Load a config """
+    """Load a configuration."""
     config_name = config
     config = configs[config]
     nic = get_nic()
@@ -176,17 +177,19 @@ def load(config):
             break
         time.sleep(1)
     else:
-        click.echo('\nTimeout')
+        click.echo("\nTimeout")
 
 
 def main():
+    """Entrypoint."""
     global configs
-    if os.path.isfile(CONFIG_FILENAME):
-        with open(CONFIG_FILENAME, 'rb') as f:
+    config_filepath = CONFIG_FILENAME
+    if os.path.isfile(config_filepath):
+        with open(config_filepath, "rb") as f:
             configs = pickle.load(f)
 
     cli()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
